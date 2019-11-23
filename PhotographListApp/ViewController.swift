e7cc4cb5a6a7aa4425ac572f47bf59566330541d
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-11-23 20:02:44 -0300

    add api consumption

--- PhotographListApp/ViewController.swift
+++ PhotographListApp/ViewController.swift
@@ -12,7 +12,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fetchJsonList{(res) in
+            switch res {
+            case .success(let photographs):
+                photographs.forEach({ (photograph) in
+                    print(photograph.author)
+                })
+            case .failure(let err):
+                print("Failed to fetch photographs:", err)
+            }
+        }
+        
+//        fetchJsonList{(photographs, err) in
+//            if let err = err{
+//                print("Failed to fetch photographs:", err)
+//                return
+//            }
+//
+//            photographs?.forEach({ (photograph) in
+//                print(photograph.author)
+//            })
+//        }
+    }
+    
+    fileprivate func fetchJsonList(completion: @escaping (Result<[Photograph], Error>) -> ()){
+        let urlString = "https://picsum.photos/v2/list?page=2&limit=100"
+        
+        guard let url = URL(string: urlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+                
+            if let err = err{
+                completion(.failure(err))
+                return
+            }
+            
+            do{
+                 let photographs = try JSONDecoder().decode([Photograph].self, from: data!)
+                completion(.success(photographs))
+            }catch let jsonError{
+                completion(.failure(jsonError))
+            }
+           
+            
+        }.resume()
+        
     }
 
 
